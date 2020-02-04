54a83fd27a457d13452db9b81e29e42a45f7acf2
younghokim <graycoral.kim@gmail.com>
2020-02-05 00:34:34 +0900

    [Jungol] 1257_전깃줄(중)-KYH

---
+++ JUNGOL/1257_전깃줄(중).cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+const int MAXN = 1e6+10;
+
+int N;
+vector<int> path;
+vector<pair<int,int>> Line;
+int D[MAXN];
+
+void input()
+{
+    cin >> N;
+    for(int i=0; i<N;i++){
+        int tmp1, tmp2;
+        cin >> tmp1 >> tmp2;
+        Line.push_back(make_pair(tmp1, tmp2));
+    }
+}
+
+int BS(int s, int e, int v)
+{
+    int ret = 0;
+    while(s<=e){
+        int m = (s+e)/2;
+        if(Line[D[m]].second < v) {
+            s = m +1;
+            ret = s;
+        } else {
+            e = m -1;
+        }
+    }
+    return ret;
+}
+
+void sol()
+{
+    int cur = 0;
+
+    sort(Line.begin(), Line.end());
+    for(int i=0; i<N; i++){
+        int idx = BS(cur, i, Line[i].second);
+        if(idx > cur){
+            path.pop_back();
+            path.push_back(idx);
+        }
+        else {
+            D[cur++] = idx;
+            path.push_back(idx);
+        }
+    }
+    cout << cur << endl;
+    for(auto p: path){
+        cout << p << endl;
+    }
+}
+
+int main()
+{
+    input();
+    sol();
+    return 0;
+}
