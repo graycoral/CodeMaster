f70584b89bff395439a2f6b6362874113ccb5165
Youngho Kim <graycoral@nate.com>
2021-03-30 23:30:06 +0900

    [LGECODEPRO] Add PreCodeJam #A -fail

---
+++ SS_CodeMaster/LGE_CodeMaster_2103_A.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct pos {
+	int r, c;
+} pos;
+
+char map[3][10]{
+	{'Q','W','E','R','T','Y','U','I','O','P'},
+	{'A','S','D','F','G','H','J','K','J','L'},
+	{'Z','X','C','V','B','N','M'}
+};
+
+int cnt = 0;
+char input[100 + 1];
+
+void init()
+{
+	for (int i = 0; i < sizeof(input); i++) {
+		input[i] = 0;
+	}
+	cin >> input;
+	cnt = 1;
+}
+
+pos GetPos(char ch)
+{
+	pos ret = {0,0};
+	
+	for (int r = 0; r < 3; r++) {
+		for (int c = 0; c < 10; c++) {
+			if (map[r][c] == ch) {
+				ret.r = r; ret.c = c;
+				return ret;
+			}
+		}
+	}
+
+	return ret;
+}
+
+void countKey()
+{
+	int curIdx = 0;	
+
+	while (input[curIdx+1] != 0) {
+		pos cur = GetPos(input[curIdx]);
+		pos next = GetPos(input[curIdx + 1]);
+		pos newPos = { 0,0 };
+
+		while (!(newPos.r == next.r && newPos.c == next.c)) {
+			if (cur.r < next.r) {
+				newPos.r = cur.r + 1;
+			}
+			else if (cur.r == next.r) {
+				newPos.r = cur.r;
+			}
+			else if (cur.r > next.r) {
+				newPos.r = cur.r - 1;
+			}
+
+			if (cur.c < next.c) {
+				newPos.c = cur.c + 1;
+			}
+			else if (cur.c == next.c) {
+				newPos.c = cur.c;
+			}
+			else if (cur.c > next.c) {
+				newPos.c = cur.c - 1;
+			}
+
+			cnt += 2;
+			if (newPos.r == next.r && newPos.c == next.c) {
+				curIdx += 1;
+				cnt += 1;
+			}
+			else {
+				cur.r = newPos.r; cur.c = newPos.c;
+			}			
+		}
+	}
+}
+
+
+int main()
+{
+	int T;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	freopen("LGE_CodeMaster_2103_A.txt", "r", stdin);
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		init();
+		countKey();
+		cout << cnt << endl;
+	}
+
+	return 0;
+}
---
+++ SS_CodeMaster/모의고사_핀볼_sol.cc
@@ -0,0 +1,111 @@
+#include <stdio.h>
+
+const int maxn = 115;
+
+#define BLOCK_NUMBER 6
+#define DIRECTION 4
+
+int map[maxn][maxn];
+int wormholePos[5][2];
+
+int ans;
+int T, N;
+
+const int dy[] = {0,0,1,-1};
+const int dx[] = {1,-1,0,0 };
+const int changeDirection[BLOCK_NUMBER][DIRECTION] = {
+	{0, 1, 2, 3},
+	{1, 3, 0, 2},
+	{1, 2, 3, 0},
+	{2, 0, 3, 1},
+	{3, 0, 1, 2},
+	{1, 0, 1, 2},
+};
+
+void read() {
+	scanf("%d", &N);
+	for(int i = 1; i<=N; ++i) {
+		for(int j=1; j<=N; ++j) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+}
+
+void setUptheWall()
+{
+	for(int i=0; i<=N; ++i) {
+		map[i][0] = map[i][N+1] = map[0][i] = map[N+1][i] = 5;
+	}
+}
+
+void wormhole(int &ny, int &nx, int wormhole_number)
+{
+	for(int i=1; i<=N ; ++i) {
+		for(int j=1; j<=N; ++j) {
+			if((ny != i || nx != j) && map[i][j] == wormhole_number) {
+				ny= i; nx=j;
+				return;
+			}
+		}
+	}
+}
+
+void simulate(int sy, int sx, int d)
+{
+	int y=sy;
+	int x=sx;
+
+	int cnt = 0;
+
+	while(1) {
+		int ny = y + dy[d];
+		int nx = x + dx[d];
+
+		if((nx == sx && ny == sy) || map[ny][nx] == -1) {
+			if(ans < cnt) ans = cnt;
+			return;
+		}
+
+		if(map[ny][nx]) {
+			if(map[ny][nx] < 6) {
+				d = changeDirection[map[ny][nx]][d];
+				cnt++;
+			}
+			else {
+				wormhole(ny, nx, map[ny][nx]);
+			}
+		}
+
+		x = nx;
+		y=  ny;
+	}
+}
+
+void sol()
+{
+	ans = 0;
+	for(int i=1; i<=N; i++) {
+		for(int j =1; j<=N; j++) {
+			for(int d = 0; d<DIRECTION; d++	) {
+				simulate(i, j, d);
+			}
+		}
+	}
+}
+
+int main()
+{
+	freopen("pin_ball_input_1.txt", "r", stdin);
+	scanf("%d", &T);
+
+	for (int test_case = 1; test_case <= T; ++test_case)
+	{
+		read();
+		setUptheWall();
+		sol();
+		printf("#%d %d\n", test_case, ans);
+	}
+
+	return 0;
+}
+
