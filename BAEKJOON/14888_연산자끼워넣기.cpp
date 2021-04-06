234df790f9a3a02c801562b13704fcd4c4a7ec52
Youngho Kim <graycoral@nate.com>
2021-04-06 19:42:16 +0900

    [BAEJUN] 14888 fail

--- BAEKJOON/14888_연산자끼워넣기.cpp
+++ BAEKJOON/14888_연산자끼워넣기.cpp
@@ -7,39 +7,67 @@ int N;
 int num[maxn];
 int cal[4];
 int total_cnt;
+int maxSum;
+int minSum = 0xfffe;
 
 
-int sol(int num, int calIdx)
+int calSum(int sum, int nextIdx)
 {
-    if(cal[calIdx] == 0)    return num;
-    else return num*cal[calIdx];
+    if(nextIdx >= N) return sum;
+
+    for(int i=0; i<4; i++) {
+        if(cal[i] == 0) continue;
+        cal[i]--;
+        if(i == 0) {
+            sum += num[nextIdx];
+            sum = calSum(sum, nextIdx +1);
+        }
+        else if(i == 1) {
+            sum -= num[nextIdx];
+            sum = calSum(sum, nextIdx +1);
+        }
+        else if(i == 2) {
+            sum *= num[nextIdx];
+            sum = calSum(sum, nextIdx +1);
+        }
+        else if(i == 3) {
+            sum /= num[nextIdx];
+            sum = calSum(sum, nextIdx +1);
+        }
+        cal[i]++;
+    }
+    maxSum = maxSum < sum ? sum : maxSum;
+    minSum = minSum > sum ? sum : minSum;
+
+    return sum;
 }
 
 int main()
 {
-    freopen("14888_연산자끼워넣기.txt", "r", stdin);
-    int T;
-    cin >> T;
+    //freopen("../input/14888_연산자끼워넣기.txt", "r", stdin);
+    int T = 1;
+    //cin >> T;
 
     for(int i=0; i<T; i++) {
         int tmp;
+		minSum = 0xfffe;
+		maxSum = 0;
         cin >> N;
-        cin >> tmp;
-        num[i] = tmp;
+        for(int j = 0; j < N; j++) {
+            cin >> tmp;
+            num[j] = tmp;
+        }
+
+        for(int calIdx=0; calIdx<4; calIdx++){
+            int tmpcal;
+            cin >> tmpcal;
+            cal[calIdx] = tmpcal;
+        }
+		
+		calSum(num[0], 1);
+
+        cout << maxSum << endl;
+        cout << minSum << endl;
     }
-
-    for(int i=0; i<4; i++){
-        int tmpcal;
-        cin >> tmpcal;
-        cal[i] = tmpcal;
-        total_cnt++;
-    }
-
-    for(int i=0; i<4; i++) {
-
-    }
-
-
-
     return 0;
 }
